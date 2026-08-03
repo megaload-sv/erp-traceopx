32b96f16bfe34fe22555240faca300bb817d501e
José Luis Reyes <joseluis.r85@gmail.com>
2026-08-03 10:26:49 -0600

    data: add MH department catalog

---
+++ app/Database/Seeds/data/mh_departments.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    ['code' => '00', 'name' => 'Otro (Para extranjeros)'],
+    ['code' => '01', 'name' => 'Ahuachapán'],
+    ['code' => '02', 'name' => 'Santa Ana'],
+    ['code' => '03', 'name' => 'Sonsonate'],
+    ['code' => '04', 'name' => 'Chalatenango'],
+    ['code' => '05', 'name' => 'La Libertad'],
+    ['code' => '06', 'name' => 'San Salvador'],
+    ['code' => '07', 'name' => 'Cuscatlán'],
+    ['code' => '08', 'name' => 'La Paz'],
+    ['code' => '09', 'name' => 'Cabañas'],
+    ['code' => '10', 'name' => 'San Vicente'],
+    ['code' => '11', 'name' => 'Usulután'],
+    ['code' => '12', 'name' => 'San Miguel'],
+    ['code' => '13', 'name' => 'Morazán'],
+    ['code' => '14', 'name' => 'La Unión'],
+];
