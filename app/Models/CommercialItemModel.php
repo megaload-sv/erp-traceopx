4486c7e7b43009a814353232558362f3945464e7
José Luis Reyes <joseluis.r85@gmail.com>
2026-08-06 11:01:47 -0600

    feat: support normalized catalog groups and units

--- app/Models/CommercialItemModel.php
+++ app/Models/CommercialItemModel.php
@@ -6,5 +6,10 @@ class CommercialItemModel extends BaseModel
 {
     protected $table = 'commercial_items';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['uuid', 'code', 'item_type', 'name', 'long_description', 'default_unit_id', 'suggested_price', 'allows_price_override', 'status', 'entry_user', 'modify_user', 'delete_user'];
+    protected $allowedFields = [
+        'uuid', 'code', 'item_type', 'item_group_id', 'name', 'long_description',
+        'default_unit_id', 'suggested_price', 'allows_price_override',
+        'allows_unit_override', 'display_order', 'source_reference',
+        'normalization_notes', 'status', 'entry_user', 'modify_user', 'delete_user',
+    ];
 }
