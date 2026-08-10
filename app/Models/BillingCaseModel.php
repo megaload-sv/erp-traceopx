5282a7ddd0d7da59657b816e027d8afb5711b6bc
José Luis Reyes <joseluis.r85@gmail.com>
2026-08-09 20:58:41 -0600

    feat: add billing origin type

--- app/Models/BillingCaseModel.php
+++ app/Models/BillingCaseModel.php
@@ -7,7 +7,7 @@ class BillingCaseModel extends BaseModel
     protected $table = 'billing_cases';
     protected $primaryKey = 'id';
     protected $allowedFields = [
-        'uuid','code','service_case_id','work_order_id','quotation_id','customer_id','payment_term_id',
+        'uuid','code','origin_type','service_case_id','work_order_id','quotation_id','customer_id','payment_term_id',
         'document_type','status','currency_code','quotation_total_snapshot','invoiceable_amount','paid_amount',
         'balance_amount','payment_term_code_snapshot','payment_term_name_snapshot','notes','prepared_by_user_id',
         'prepared_at','entry_user','modify_user','delete_user'
