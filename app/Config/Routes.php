e9f9e1e9f962ca0c8b3ebd6be85969c65ec9cc90
José Luis Reyes <joseluis.r85@gmail.com>
2026-08-10 00:34:10 -0600

    feat(dte): route fiscal classification updates

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -96,6 +96,7 @@ $routes->group('', ['filter' => 'auth'], static function (RouteCollection $route
         $routes->get('', 'BillingController::index', ['as' => 'billing.index']);
         $routes->post('service-case/(:num)/prepare', 'BillingController::prepare/$1', ['as' => 'billing.prepare']);
         $routes->get('(:num)', 'BillingController::show/$1', ['as' => 'billing.show']);
+        $routes->post('(:num)/items/(:num)/tax', 'BillingController::updateItemTax/$1/$2', ['as' => 'billing.items.tax.update']);
     });
 
     $routes->group('dte-settings', static function (RouteCollection $routes): void {
@@ -138,13 +139,11 @@ $routes->group('', ['filter' => 'auth'], static function (RouteCollection $route
         $routes->get('(:num)', 'CustomersController::show/$1', ['as' => 'customers.show']);
         $routes->get('(:num)/edit', 'CustomersController::edit/$1', ['as' => 'customers.edit']);
         $routes->post('(:num)', 'CustomersController::update/$1', ['as' => 'customers.update']);
-
         $routes->get('(:num)/contacts/create', 'CustomerContactsController::create/$1', ['as' => 'customers.contacts.create']);
         $routes->post('(:num)/contacts', 'CustomerContactsController::store/$1', ['as' => 'customers.contacts.store']);
         $routes->get('(:num)/contacts/(:num)/edit', 'CustomerContactsController::edit/$1/$2', ['as' => 'customers.contacts.edit']);
         $routes->post('(:num)/contacts/(:num)', 'CustomerContactsController::update/$1/$2', ['as' => 'customers.contacts.update']);
         $routes->post('(:num)/contacts/(:num)/primary', 'CustomerContactsController::makePrimary/$1/$2', ['as' => 'customers.contacts.primary']);
-
         $routes->get('(:num)/addresses/create', 'CustomerAddressesController::create/$1', ['as' => 'customers.addresses.create']);
         $routes->post('(:num)/addresses', 'CustomerAddressesController::store/$1', ['as' => 'customers.addresses.store']);
         $routes->get('(:num)/addresses/(:num)/edit', 'CustomerAddressesController::edit/$1/$2', ['as' => 'customers.addresses.edit']);
