08b54e0d4b522131fd6db157752518298a384248
José Luis Reyes <joseluis.r85@gmail.com>
2026-08-10 01:01:07 -0600

    feat(dte): add receiver update route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -96,6 +96,7 @@ $routes->group('', ['filter' => 'auth'], static function (RouteCollection $route
         $routes->get('', 'BillingController::index', ['as' => 'billing.index']);
         $routes->post('service-case/(:num)/prepare', 'BillingController::prepare/$1', ['as' => 'billing.prepare']);
         $routes->get('(:num)', 'BillingController::show/$1', ['as' => 'billing.show']);
+        $routes->post('(:num)/receiver', 'BillingController::updateReceiver/$1', ['as' => 'billing.receiver.update']);
         $routes->post('(:num)/items/(:num)/tax', 'BillingController::updateItemTax/$1/$2', ['as' => 'billing.items.tax.update']);
     });
 
